6521513dcf39eb86c3be72c1cbf22c6e5b932acd
mahi <mahi@10.1.10.105>
2018-10-18 14:03:56 -0500

    Added a space

--- Assessment_02-08-2018/src/main/java/com/assessment/application/Application.java
+++ Assessment_02-08-2018/src/main/java/com/assessment/application/Application.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
+//adding a comment
 @SpringBootApplication
 @ComponentScan("com.assessment")
 public class Application {
